c0915ff14c2d495651cbab62fd59ea5a19008a9e
vijay2181 <66196388+vijay2181@users.noreply.github.com>
2022-09-20 22:12:52 +0530

    Update remote-userdata.sh

--- remote-userdata.sh
+++ remote-userdata.sh
@@ -4,3 +4,8 @@ GIT_TOKEN=$(aws ssm get-parameter --name /github/repo/token --with-decryption --
 [ ! -d /home/ec2-user/data ] && git clone https://${GIT_TOKEN}@github.com/vijay2181/packer-gitclone-test.git /home/ec2-user/data || ( cd /home/ec2-user/data ; git pull )
 
 bash /home/ec2-user/data/master.sh
+
+#we can read tags in user data when provisioning dynamic ec2 instance
+InstanceId=$(wget -qO- http://instance-data/latest/meta-data/instance-id)
+Region=$(wget -qO- http://instance-data/latest/meta-data/placement/region)
+aws ec2 describe-tags --region $Region --filter "Name=resource-id,Values=$InstanceId" --output=text | sed -r 's/TAGS\t(.*)\t.*\t.*\t(.*)/\1="\2"/' > /home/ec2-user/ec2-tags
